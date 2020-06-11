19c79be111712a074f7c2babd012722f1254c12f
akatul2885 <33487560+akatul2885@users.noreply.github.com>
2020-06-12 00:12:04 +0530

    Update SpringBootTomcatApplication.java

--- src/main/java/com/tomcat/SpringBootTomcatApplication.java
+++ src/main/java/com/tomcat/SpringBootTomcatApplication.java
@@ -2,18 +2,14 @@ package com.tomcat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class SpringBootTomcatApplication extends SpringBootServletInitializer{
+public class SpringBootTomcatApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTomcatApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringBootTomcatApplication.class);
-	}
+	
 }
