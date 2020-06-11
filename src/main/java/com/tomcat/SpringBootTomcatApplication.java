113b9975fbc3de1a8c11629a284cfef7c2811d8f
akatul2885 <33487560+akatul2885@users.noreply.github.com>
2020-06-11 23:38:52 +0530

    Add files via upload

---
+++ src/main/java/com/tomcat/SpringBootTomcatApplication.java
@@ -0,0 +1,19 @@
+package com.tomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootTomcatApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTomcatApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootTomcatApplication.class);
+	}
+}
